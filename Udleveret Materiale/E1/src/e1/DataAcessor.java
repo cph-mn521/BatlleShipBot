c5e1423600a2b86b3f51a91057c29dc31f7bf106
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2018-12-06 15:53:25 +0100

    E1 upd to collect data

---
+++ Udleveret Materiale/E1/src/e1/DataAcessor.java
@@ -0,0 +1,182 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Martin Wulff
+ */
+public class DataAcessor {
+    private final String FileName;
+    private File fil;
+
+    /**
+     * Constructor for the class, used to assign the current database.
+     *
+     * @param FileName The File to pull data from.
+     *
+     */
+    public DataAcessor(String FileName) {
+        fil = new File(FileName);
+        this.FileName = FileName;
+    }
+
+    /**
+     * Method for retrieving all entries in the current selected txt document.
+     * Will not return blank entries
+     *
+     * @throws delfinen.data.DataException
+     */
+
+    public List<String> getEntries() throws DataException {
+        String line = null;
+        List<String> Output = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FileName));
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals("")) {
+                    Output.add(line);
+                }
+            }
+            return Output;
+        } catch (IOException e) {
+//            throw new DataException();
+            return Output;
+        }
+    }
+
+    /**
+     * Method for searching for keywords in the database. Searches the database
+     * for all entries with containing the query.
+     *
+     * For more precise searches, knowledge of the data structure is required.
+     *
+     * @param query The wanted Query.
+     * @return machingEntries All entries that contains the query.
+     * @throws DataException
+     *
+     */
+
+    public List<String> searchEntries(String query) throws DataException {
+        String line = null;
+        List<String> matchingEntries = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FileName));
+            System.out.println(query);
+            while ((line = reader.readLine()) != null) {
+                if (line.matches(query)) {
+                    matchingEntries.add(line);
+                } else if (line.contains(query)) {
+                    matchingEntries.add(line);
+                }
+            }
+            return matchingEntries;
+        } catch (IOException e) {
+//            throw new DataException();
+            if (matchingEntries.isEmpty()) {
+                return (this.getEntries());
+            } else {
+                return matchingEntries;
+            }
+        }
+    }
+
+    /**
+     * Method for adding a new entry to the currently selected file. New entry
+     * is always appended, and will appear last in the database.
+     *
+     * @param obj The object to add to the database
+     * @throws DataException
+     *
+     */
+    
+    public void newDataEntry(){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FileName, true));
+            writer.newLine();
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+        }
+    }
+    
+    public void append(String in) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FileName, true));
+            writer.write(in+",");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            
+        }
+        
+    }
+    public void append(String in,int i) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FileName, true));
+            writer.write(in);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            
+        }
+        
+    }
+    
+    
+    
+    public void addEntry(String obj) throws DataException {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FileName, fil.exists()));
+            writer.newLine();
+            writer.write(obj);
+            writer.close();
+        } catch (IOException e) {
+            throw new DataException();
+        }
+    }
+
+    /**
+     * Method for editing an entry in the database. Works by finding the object
+     * to change, and then we writing the file to match the new signature. Takes
+     * 2 parameters, both of string type.
+     *
+     * @param old The entry to change.
+     * @param N The changed entry.
+     * @throws DataException
+     *
+     */
+
+    public void editEntry(String old, String N) throws DataException {
+        try {
+            List<String> newFile = getEntries();
+            String lines = "";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FileName)); //Append when file exists, make when false
+            for (String string : newFile) {
+                if (string.length()>7 && string.contains(old.substring(3, old.length()-3))) { //changed equals -> contains to handle compmembers
+                    lines += N + "\r\n";
+                } else {
+                    lines += string + "\r\n";
+                }
+            }
+            writer.write(lines);
+            writer.flush();
+            writer.close();
+
+        } catch (IOException e) {
+            throw new DataException();
+        }
+    }
+}
--- Udleveret Materiale/E1/src/e1/RandomPlayer.java
+++ Udleveret Materiale/E1/src/e1/RandomPlayer.java
@@ -15,64 +15,77 @@ import java.util.Random;
  *
  * @author Tobias
  */
-public class RandomPlayer implements BattleshipsPlayer
-{
+public class RandomPlayer implements BattleshipsPlayer {
+
     private final static Random rnd = new Random();
     private int sizeX;
     private int sizeY;
-   
-    public RandomPlayer()
-    {
+    private DataAcessor DA = new DataAcessor("Data.txt");
+    private int fleetblocks;
+    private int nrOfShips;
+
+    public RandomPlayer() {
     }
-    
+
     /**
-     * Called in the beginning of each match to inform about the number of 
+     * Called in the beginning of each match to inform about the number of
      * rounds being played.
+     *
      * @param rounds int the number of rounds i a match
      */
     @Override
-    public void startMatch(int rounds, Fleet ships, int sizeX, int sizeY)
-    {
+    public void startMatch(int rounds, Fleet ships, int sizeX, int sizeY) {
+        DA.newDataEntry();
         this.sizeX = sizeX;
+        DA.append("" + sizeX);
         this.sizeY = sizeY;
+        DA.append("" + sizeY);
+        nrOfShips = ships.getNumberOfShips();
+        DA.append("" + nrOfShips);
+        fleetblocks = 0;
+        for (Ship ship : ships) {
+            fleetblocks += ship.size();
+        }
+        DA.append("" + fleetblocks);
     }
 
-   
     /**
-     * The method called when its time for the AI to place ships on the board 
+     * The method called when its time for the AI to place ships on the board
      * (at the beginning of each round).
-     * 
-     * The Ship object to be placed  MUST be taken from the Fleet given 
-     * (do not create your own Ship objects!).
-     * 
-     * A ship is placed by calling the board.placeShip(..., Ship ship, ...) 
-     * for each ship in the fleet (see board interface for details on placeShip()).
-     * 
-     * A player is not required to place all the ships. 
-     * Ships placed outside the board or on top of each other are wrecked.
-     * 
-     * @param fleet Fleet all the ships that a player should place. 
+     *
+     * The Ship object to be placed MUST be taken from the Fleet given (do not
+     * create your own Ship objects!).
+     *
+     * A ship is placed by calling the board.placeShip(..., Ship ship, ...) for
+     * each ship in the fleet (see board interface for details on placeShip()).
+     *
+     * A player is not required to place all the ships. Ships placed outside the
+     * board or on top of each other are wrecked.
+     *
+     * @param fleet Fleet all the ships that a player should place.
      * @param board Board the board were the ships must be placed.
      */
     @Override
-    public void placeShips(Fleet fleet, Board board)
-    {
-        for(int i = 0; i < fleet.getNumberOfShips(); ++i)
-        {
+    public void placeShips(Fleet fleet, Board board) {
+        for (int i = 0; i < fleet.getNumberOfShips(); ++i) {
             Ship s = fleet.getShip(i);
+            DA.append(""+s.size());
             boolean vertical = rnd.nextBoolean();
             Position pos;
-            if(vertical)
-            {
+            if (vertical) {
                 int x = rnd.nextInt(sizeX);
-                int y = rnd.nextInt(sizeY-(s.size()-1));
+                int y = rnd.nextInt(sizeY - (s.size() - 1));
                 pos = new Position(x, y);
-            }
-            else
-            {
-                int x = rnd.nextInt(sizeX-(s.size()-1));
+                DA.append("" + x);
+                DA.append("" + y);
+                DA.append("1");
+            } else {
+                int x = rnd.nextInt(sizeX - (s.size() - 1));
                 int y = rnd.nextInt(sizeY);
                 pos = new Position(x, y);
+                DA.append("" + x);
+                DA.append("" + y);
+                DA.append("0");
             }
             board.placeShip(pos, s, vertical);
         }
@@ -80,96 +93,105 @@ public class RandomPlayer implements BattleshipsPlayer
 
     /**
      * Called every time the enemy has fired a shot.
-     * 
-     * The purpose of this method is to allow the AI to react to the 
-     * enemy's incoming fire and place his/her ships differently next round.
-     * 
-     * @param pos Position of the enemy's shot 
+     *
+     * The purpose of this method is to allow the AI to react to the enemy's
+     * incoming fire and place his/her ships differently next round.
+     *
+     * @param pos Position of the enemy's shot
      */
     @Override
-    public void incoming(Position pos)
-    {
+    public void incoming(Position pos) {
+        
+        DA.append("e "+pos.x);
+        DA.append("e "+pos.y);
         //Do nothing
     }
 
-    
     /**
      * Called by the Game application to get the Position of your shot.
-     *  hitFeedBack(...) is called right after this method.
-     * 
-     * @param enemyShips Fleet the enemy's ships. Compare this to the Fleet 
-     * supplied in the hitFeedBack(...) method to see if you have sunk any ships.
-     * 
+     * hitFeedBack(...) is called right after this method.
+     *
+     * @param enemyShips Fleet the enemy's ships. Compare this to the Fleet
+     * supplied in the hitFeedBack(...) method to see if you have sunk any
+     * ships.
+     *
      * @return Position of you next shot.
      */
     @Override
-    public Position getFireCoordinates(Fleet enemyShips)
-    {
+    public Position getFireCoordinates(Fleet enemyShips) {
         int x = rnd.nextInt(sizeX);
         int y = rnd.nextInt(sizeY);
-        return new Position(x,y);
+        DA.append("m "+x);
+        DA.append("m "+y);
+        return new Position(x, y);
     }
 
-    
     /**
      * Called right after getFireCoordinates(...) to let your AI know if you hit
-     * something or not. 
-     * 
-     * Compare the number of ships in the enemyShips with that given in 
+     * something or not.
+     *
+     * Compare the number of ships in the enemyShips with that given in
      * getFireCoordinates in order to see if you sunk a ship.
-     * 
+     *
      * @param hit boolean is true if your last shot hit a ship. False otherwise.
      * @param enemyShips Fleet the enemy's ships.
      */
     @Override
-    public void hitFeedBack(boolean hit, Fleet enemyShips)
-    {
+    public void hitFeedBack(boolean hit, Fleet enemyShips) {
+        if (hit) {
+           DA.append("1"); 
+        }
+        else DA.append("0");
+        
         //Do nothing
-    }    
+    }
 
-    
-    
-    
-    
     /**
      * Called at the beginning of each round.
+     *
      * @param round int the current round number.
      */
     @Override
-    public void startRound(int round)
-    {
-       
+    public void startRound(int round) {
+        DA.newDataEntry();
+        DA.append("" + sizeX);
+        DA.append("" + sizeY);
+        DA.append("" + nrOfShips);
+        DA.append("" + fleetblocks);
     }
 
-    
     /**
      * Called at the end of each round to let you know if you won or lost.
      * Compare your points with the enemy's to see who won.
-     * 
-     * @param round int current round number.
-     * @param points your points this round: 100 - number of shot used to sink 
-     * all of the enemy's ships. 
      *
-     * @param enemyPoints int enemy's points this round. 
+     * @param round int current round number.
+     * @param points your points this round: 100 - number of shot used to sink
+     * all of the enemy's ships.
+     *
+     * @param enemyPoints int enemy's points this round.
      */
     @Override
-    public void endRound(int round, int points, int enemyPoints)
-    {
-        //Do nothing
+    public void endRound(int round, int points, int enemyPoints) {
+        
+        DA.append(""+points);
+        DA.append(""+enemyPoints);
+        if(points>enemyPoints) DA.append("1",1);
+        else DA.append("0",1);
+               
+       
+        //Do nothing endgame stuff here
     }
-    
-    
+
     /**
-     * Called at the end of a match (that usually last 1000 rounds) to let you 
+     * Called at the end of a match (that usually last 1000 rounds) to let you
      * know how many losses, victories and draws you scored.
-     * 
+     *
      * @param won int the number of victories in this match.
      * @param lost int the number of losses in this match.
      * @param draw int the number of draws in this match.
      */
     @Override
-    public void endMatch(int won, int lost, int draw)
-    {
-        //Do nothing
+    public void endMatch(int won, int lost, int draw) {
+        //Do nothing not of wurf to our data.
     }
 }
