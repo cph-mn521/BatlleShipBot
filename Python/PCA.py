27147ad8791dc712c883cad7f96847e9bcfa5bfd
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2018-12-11 13:56:12 +0100

    Update PCA.py

--- Python/PCA.py
+++ Python/PCA.py
@@ -12,6 +12,20 @@ import os
 os.chdir("C:\\Users\\Martin Wulff\\Documents\\BattelShipBot\\BatlleShipBot\\Python")
 
 
+#Finding all won games
+results = game_data['matchResult']
+wongames  = []
+lostgames = []
+tiedgames = []
+for i in range(0,len(results)):
+    if results[i] < 0:
+        lostgames.append(i)
+    if results[i] > 0:
+        wongames.append(i)
+    if results[i] == 0:
+        tiedgames.append(i)
+
+
 # Importing Data:
 input = pd.read_csv('Data.txt')
 data = input.split();
@@ -20,18 +34,14 @@ Index = S[0]
 Data = data[1:,:];
 df = game_data.as_matrix
 
+
+
+
+
+
+
+#svd
 u, s, v = np.linalg.svd(game_data)
-
-
-u
-
-# Finding Eigen Values.
-u
-Eigenval = np.zeros(u.length())
-print('Eigenvalues in descending order:')
-for i in range(0, u.length()):
-    Eigenval[i]= u[i,i]
-    print(Eigenval[i])
 
 
 #Plot of eigenvalue and caputred variance
@@ -44,9 +54,16 @@ plt.bar(range(1,len(var_exp)+1),var_exp)
 
 #Projecting Data onto the first 3 principal components.
 vectors = v[0:2,:]
+
 Y = game_data.dot(np.transpose(vectors))
+T1 = Y.loc[wongames]
+T2 = Y.loc[tiedgames]
+T3 = Y.loc[lostgames]
 fig = plt.figure()
-plt.scatter(Y[0],Y[1])
+
+plt.scatter(T3[0],T3[1],color='blue')
+plt.scatter(T2[0],T2[1],color='green')
+plt.scatter(T1[0],T1[1],color='red')
 plt.grid()
 plt.show()
 
@@ -54,7 +71,9 @@ plt.show()
 
 
 #PCA for own shot data.
-u, s, v = np.linalg.svd(own_shot_data)
+ShotData = own_shot_data.fillna(0)
+u, s, v = np.linalg.svd(ShotData)
+
 
 #Plot of eigenvalue and caputred variance
 tot = sum(s)
@@ -66,10 +85,70 @@ plt.bar(range(1,len(var_exp)+1),var_exp)
 
 #Projecting Data onto the first 3 principal components.
 vectors = v[0:2,:]
-Y = game_data.dot(np.transpose(vectors))
+Y = ShotData.dot(np.transpose(vectors))
+T1 = Y.loc[wongames]
+T2 = Y.loc[tiedgames]
+T3 = Y.loc[lostgames]
 fig = plt.figure()
-plt.scatter(Y[0],Y[1])
+
+plt.scatter(T3[0],T3[1],color='blue')
+plt.scatter(T2[0],T2[1],color='green')
+plt.scatter(T1[0],T1[1],color='red')
 plt.grid()
 plt.show()
 
+
+#PCA forcombined data
+
+frames = [game_data,ShotData]
+DATA = pd.concat(frames)
+u, s, v = np.linalg.svd(DATA)
+
+
+#Plot of eigenvalue and caputred variance
+tot = sum(s)
+var_exp = [(i / tot)*100 for i in sorted(s, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+
+plt.bar(range(1,len(var_exp)+1),var_exp)
+
+
+#Projecting Data onto the first 3 principal components.
+Y = DATA.dot(np.transpose(vectors))
+T1 = Y.loc[wongames]
+T2 = Y.loc[tiedgames]
+T3 = Y.loc[lostgames]
+fig = plt.figure()
+
+plt.scatter(T3[0],T3[1],color='blue')
+plt.scatter(T2[0],T2[1],color='green')
+plt.scatter(T1[0],T1[1],color='red')
+plt.grid()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+test =np.matrix([[1, 2],[2, 4],[3, 4],[3, 5],[5, 6]])
+ind = [0,2,4]
+
+
+
+
+test[ind]
+
+
+Y.as_matrix[ind]
 
